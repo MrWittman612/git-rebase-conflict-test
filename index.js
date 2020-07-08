9cd5619c70f8d9d27e52715e08c688da4a22f826
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-08 02:48:11 -0500

    broken function

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ function exponentiation(num1, num2) {
 }
 
 console.log(exponentiation(3, 5));
+
+// test how long a function takes to run
+
+function printElapsedTime(fTest) {
+	let sT = Date.now(),
+		vReturn = fTest(),
+		eT = Date.now();
+
+	console.log(`Elapsed time: ${String(eT - sT)} milliseconds`);
+	return vReturn;
+}
+
+const doWork = add(2, 3);
+
+const timeAdd = printElapsedTime(doWork);
+
+console.log(timeAdd);
