c1b15950b2ceee4cdce2baf4e744592b315a8f8d
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-08 01:10:43 -0500

    added add function

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ console.log('hello word');
 console.log('commit 2 master');
 console.log('second change side branch 1');
 console.log('commit 3 side branch');
+
+function add(a, b) {
+	return a + b;
+}
+
+const num = add(2, 3);
+
+console.log(num);
