cd3bff25a49bb81baa55505cea71127092c17ddb
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-07-08 01:21:45 -0500

    added subtrack function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log('hello word');
 console.log('commit 2 master');
 console.log('second change side branch 1');
+
+function subtract(a, b) {
+	return a - b;
+}
+
+console.log(subtract(8, 3));
